ddd07b76494bae69b13038dd7d8470c3e189ded2
Marcelearn <jaworskimarcel@gmail.com>
2023-01-19 15:25:27 +0100

    Part1 done

--- models/MagicSpell.js
+++ models/MagicSpell.js
@@ -3,12 +3,38 @@ import mongoose from "mongoose";
 const magicSpellSchema = new mongoose.Schema({
   name: { type: String, required: true },
   description: String,
-  cost: Number,
-  damage: Number,
-  castingTime: Number,
-  level: Number,
-  range: Number,
-  areaOfEffect: Boolean,
+  cost:{
+    type: Number,
+    min: 1,
+  },
+  damage:{
+    type: Number,
+    min: 0,
+    default: 0,
+  },
+  castingTime:{
+    type: Number,
+    min: 0,
+    default: 6,
+  },
+  level:{
+    type: Number,
+    min: 1,
+    default: 1,
+  },
+  range:{
+    type: Number,
+    min: 0,
+    default: 0.1,
+  },
+  areaOfEffect:{
+    type: Boolean,
+    default: false,
+  },
+  school:{
+    type: String,
+    enum: ['physical', 'arcane', 'fire', 'frost', 'nature', 'shadow', 'holy']
+  }
 });
 
 const MagicSpell = mongoose.model("MagicSpell", magicSpellSchema);
--- models/Wizard.js
+++ models/Wizard.js
@@ -1,12 +1,57 @@
 import mongoose from "mongoose";
+import { MagicSpell } from "./MagicSpell.js"
 
 const wizardSchema = new mongoose.Schema({
   name: { type: String, required: true },
   description: String,
-  age: Number,
-  level: Number,
-  accuracy: Number,
-  critical: Number,
+  age:{
+    type: Number,
+    min: 1
+  },
+  level:{
+    type: Number,
+    min: 1,
+    default: 1,
+    
+  },
+  accuracy:{
+    type: Number,
+    min: 0,
+    max: 1,
+    default: 0.3,
+  },
+  critical:{
+    type: Number,
+    min: 0,
+    max: 1,
+    default: 0,
+  },
+  origin:{
+    type: String,
+    enum: ['Ankh-Morpork','Bad Schüschein','Betrek','Borogravia','Brindisi','Chimeria','Chirm','Copperhead','Cori Celesti','Djelibeybi','Ephebe','Ghat','Hersheba','Howondaland','Klatchistan','Lipwig','Rehigreed Province','Schmaltzberg','Skund','Sto Helit','Sto Lat','Sunken Leshp','The Chalk','The Wyrmberg','Überwald']
+  },
+  energy:{
+    health:{
+      type: Number,
+      min: 0,
+      default: 1,
+    },
+    stamina:{
+      type: Number,
+      min: 0,
+      default: 10,
+    },
+    magic:{
+      type: Number,
+      min: 0,
+      default: 100,
+    },
+    
+  },
+  spells:[{
+    type: mongoose.schema.Types.ObjectId,
+    ref: MagicSpell,
+  }],
 });
 
 const Wizard = mongoose.model("Wizard", wizardSchema);
